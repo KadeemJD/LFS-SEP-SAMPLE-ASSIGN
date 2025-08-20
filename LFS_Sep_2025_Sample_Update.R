b2ea56d394dfedfa2d781f2780e75fb90d722ddb
KadeemJD <Kadeem.daniels@gmail.com>
2025-08-20 14:59:44 -0600

    addition of script to get unique csvs for surveys matched and census matched, secondly making each section more readable and making script more navigable.

--- LFS_Sep_2025_Sample_Update.R
+++ LFS_Sep_2025_Sample_Update.R
@@ -1,8 +1,12 @@
-# ------------------------------------------------
-# R Script to Assign Samples from SAV to Postgres
-# ------------------------------------------------
+# ------------------------------------------------------------------
+# ⫸⫸⫸⫸⫸R Script to Assign Samples from SAV to Postgres⫷⫷⫷⫷⫷#
+# ------------------------------------------------------------------
 
-# Load Libraries
+
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#01 Load Libraries
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+
 library(haven)        # For reading SAV
 library(DBI)          # Database interface
 library(RPostgres)    # PostgreSQL driver
@@ -12,13 +16,16 @@ library(glue)         # For string interpolation
 library(readr)        # Optional for CSV export
 library(dotenv)       # For loading .env config file
 
-# ------------------------------
-# Load Config from .env File
-# ------------------------------
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#02 Load Config from .env File
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
 
 load_dot_env(file = "config.env")
 
-# Get environment variables from main database (test2 aka surveys)
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#03-1 Get environment variables from main database (test2 aka surveys)
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+
 db_host <- Sys.getenv("DB_HOST")
 db_port <- as.integer(Sys.getenv("DB_PORT"))
 db_name <- Sys.getenv("DB_NAME")
@@ -27,19 +34,27 @@ db_password <- Sys.getenv("DB_PASSWORD")
 db_table <- Sys.getenv("DB_TABLE")
 sav_path <- Sys.getenv("SAV_PATH")
 
-# census_archives database
-#Uncomment this when you need to find the unmatched records from surveys database
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#03-2 Connection to census_archives database
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+
+#🔓~~Uncomment the code below when you need to find the unmatched records from surveys database as well as
+#section 11 (Match surveys_unmatched against post_census_2022_building) below~~ 🔓#
+
 # census_db_name <- Sys.getenv("CENSUS_DB_NAME")
 # census_table <- Sys.getenv("CENSUS_TABLE")
 
-# ------------------------------
-# Read SAV File
-# ------------------------------
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#04 Read SAV File
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
 
 cat("Reading SAV file...\n")
 sav_data <- read_sav("SEPT2025SAMPLE.sav")
 
-# Rename column for consistency
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#05 Rename column for consistency
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+
 sav_data <- sav_data %>%
   rename(ed_2022 = ed,
          blk_newn_2022=block,
@@ -53,11 +68,11 @@ sav_data <- sav_data %>% distinct(concat_key, .keep_all=TRUE)
 
 cat("Unique records in SAV file: ", n_distinct(sav_data$concat_key), "\n")
 
-# ------------------------------
-# Connect to PostgreSQL databases 
-# ------------------------------
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#06-1 Connect to PostgreSQL databases 
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
 
-#Connecting to surveys (aka test2) database
+#📁Connecting to surveys (aka test2) database📁#
 
 cat("Connecting to Surveys DB...\n")
 conn_surveys <- tryCatch({
@@ -74,8 +89,11 @@ conn_surveys <- tryCatch({
   return(NULL)
 })
 
-# #Connecting to census_archives database
-# Un- comment this when we need to make a connection to census archives database and search for unmatched records
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#06-2 Connecting to census_archives database
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+
+#🔓~~Uncomment the code below when we need to make a connection to census archives database and search for unmatched records~~🔓#
 #
 # cat("Connecting to census_archives DB...\n")
 # conn_census <- tryCatch({
@@ -93,19 +111,19 @@ conn_surveys <- tryCatch({
 # })
 
 
-# # ------------------------------
-# # Reset sampled column
-# # ------------------------------
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#07 Reset sampled column
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
 
-# Uncomment when you need to clear all the records from the column "sampled"
+#🔓~~Uncomment the code below when you need to clear all the records from the column "sampled"~~🔓#
 
 # cat("Resetting sampled flags to NULL...\n")
 # reset_query <- glue("UPDATE {db_table} SET sampled = NULL;")
 # dbExecute(conn_surveys, reset_query)
 
-# ------------------------------
-# Read from Database (aka test2)
-# ------------------------------
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#08 Read from Database (aka test2)
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
 
 cat("Fetching DB data...\n")
 query <- glue("SELECT interview__key, ed_2022, blk_newn_2022, bldg_newn FROM {db_table};")
@@ -114,9 +132,10 @@ db_data <- dbGetQuery(conn_surveys, query) %>%
     concat_key = paste(interview__key, ed_2022, blk_newn_2022, bldg_newn, sep = "-")
   )
 
-# ------------------------------
-# Match & Filter Surveys Database
-# ------------------------------
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#09 Match & Filter Surveys Database
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+
 #This is also used when we have determined which records from surveys and which records from post_census to use
 #so you can do the last match and filter
 
@@ -133,10 +152,39 @@ cat("surveys_unmatched records: ", nrow(surveys_unmatched), "\n")
 write_csv(surveys_unmatched, "surveys_unmatched_records.csv")
 write_csv(surveys_matched, "surveys_matched_records.csv")
 
-# # ------------------------------------------------------------------
-# # Match surveys_unmatched against post_census_2022_building
-# # ------------------------------------------------------------------
-# Uncomment this if you need to checked unmatched from surveys against post census
+
+
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#10 Export unique EDs in a csv separately
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+# 
+#🔓~~Uncomment the code below and run it independently when you need to get the matched unique eds from the matched records~~🔓# 
+#
+# # For surveys_matched
+# unique_surveys_eds <- surveys_matched %>%
+#   select(ed_2022) %>%
+#   distinct()
+# 
+# write_csv(unique_surveys_eds, "unique_surveys_matched_eds.csv")
+# cat(glue("✅ Exported {nrow(unique_surveys_eds)} unique EDs from surveys_matched to 'unique_surveys_matched_eds.csv'\n"))
+# 
+# # For post_census_matched, if it exists
+# if (exists("post_census_matched")) {
+#   unique_postcensus_eds <- post_census_matched %>%
+#     select(ed_2022) %>%
+#     distinct()
+#   
+#   write_csv(unique_postcensus_eds, "unique_post_census_matched_eds.csv")
+#   cat(glue("✅ Exported {nrow(unique_postcensus_eds)} unique EDs from post_census_matched to 'unique_post_census_matched_eds.csv'\n"))
+# }
+
+
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#11 Match surveys_unmatched against post_census_2022_building
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+
+#🔓~~Uncomment the code below aswell as section "03-2" (Connection to census_archives database) above 
+#if you need to check unmatched from surveys against post census~~🔓#
 # 
 # cat("Checking post_census_2022_building...\n")
 # census_query <- "SELECT interview__key, ed_2022, blk_newn_2022, bldg_newn FROM post_census_2022_building;"
@@ -150,34 +198,22 @@ write_csv(surveys_matched, "surveys_matched_records.csv")
 # cat("still unmatched after census check: ", nrow(surveys_unmatched), "\n")
 # 
 # write_csv(post_census_matched, "post_census_matched_records.csv")
-# 
-
-#Possible deletion
-# # Making sure that fields are numeric and getting rid of leading zeros
-# surveys_matched <- surveys_matched %>%
-#   mutate(blk_newn_2022 = as.character(blk_newn_2022),
-#          bldg_newn = as.character(bldg_newn))
-# 
-# post_census_matched <- post_census_matched %>%
-#   mutate(blk_newn_2022 = as.character(blk_newn_2022),
-#          bldg_newn = as.character(bldg_newn))
 
 
-#Possible deletion
-# # Combine all matched
-# all_matched <- bind_rows(surveys_matched, post_census_matched) #mics7_matched, pes_matched)
-# 
-# # Save final unmatched to CSV
-# write_csv(surveys_unmatched, "final_unmatched_records.csv")
+#🧹🧹🧹🧽🧼 Clean below
+
+#Combine all matched
+all_matched <- bind_rows(surveys_matched, post_census_matched)
+
 
 # Only proceed if all SAV records matched
 if (nrow(surveys_unmatched) == 0) {
   
   write_csv(all_matched, "all_matched_records.csv")
   
-  # --------------------------------------------------------
-  # Update Sampled Flag in DB Initial from Surveys database
-  # --------------------------------------------------------
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#12 Update Sampled Flag in DB Initial from Surveys database
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
   
   cat("Updating matched records...\n")
   
@@ -209,104 +245,18 @@ if (nrow(surveys_unmatched) == 0) {
 }
 
 
-
-# ------------------------------
-# Update from post_census_matched → back to test2 using 3-key match
-# ------------------------------
-
-cat("Linking post_census_matched back to test2 for remaining updates...\n")
-
-# Reload test2.sde.lfs_general_building with all relevant columns
-test2_query <- glue("SELECT interview__key, ed_2022, blk_newn_2022, bldg_newn FROM {db_table};")
-test2_data <- dbGetQuery(conn_surveys, test2_query)
-
-# Ensure all key fields are in same type (character preferred for joining)
-test2_data <- test2_data %>%
-  mutate(
-    ed_2022 = as.character(ed_2022),
-    blk_newn_2022 = as.character(blk_newn_2022),
-    bldg_newn = as.character(bldg_newn),
-    three_key = paste(ed_2022, blk_newn_2022, bldg_newn, sep = "-")
-  )
-
-post_census_matched <- post_census_matched %>%
-  mutate(
-    ed_2022 = as.character(ed_2022),
-    blk_newn_2022 = as.character(blk_newn_2022),
-    bldg_newn = as.character(bldg_newn),
-    three_key = paste(ed_2022, blk_newn_2022, bldg_newn, sep = "-")
-  )
-
-# Join back using 3-key and select correct version of columns
-back_matched <- inner_join(
-  post_census_matched,
-  test2_data,
-  by = "three_key"
-) %>% transmute(
-  interview__key = interview__key.y,  # from test2_data
-  ed_2022 = ed_2022.y
-)
-
-cat("Records matched back to test2 using 3-key match: ", nrow(back_matched), "\n")
-
-# ---------------------------------
-# Update Sampled Flag in test2 DB
-# ---------------------------------
-
-cat("🔁 Updating test2.sde.lfs_general_building for these remaining matches...\n")
-
-update_query_3key <- glue("
-  UPDATE {db_table}
-  SET sampled = '1'
-  WHERE interview__key = $1 AND ed_2022 = $2;
-")
-
-updated_rows_3key <- list()
-dbBegin(conn_surveys)
-for (i in 1:nrow(back_matched)) {
-  row <- back_matched[i, ]
-  tryCatch({
-    dbExecute(conn_surveys, update_query_3key, params = list(row$interview__key, row$ed_2022))
-    updated_rows_3key[[length(updated_rows_3key) + 1]] <- row
-  }, error = function(e) {
-    cat(glue("❌ Error updating row {i}: {e$message}\n"))
-  })
-}
-dbCommit(conn_surveys)
-
-# Save CSV
-if (length(updated_rows_3key) > 0) {
-  updated_df_3key <- bind_rows(updated_rows_3key)
-  write_csv(updated_df_3key, "updated_from_census_backmatch.csv")
-  cat(glue("✅ Additional updates applied using 3-key backmatch: {nrow(updated_df_3key)}\n"))
-} else {
-  cat("⚠️ No additional records were updated using 3-key backmatch.\n")
-}
-
-
-# Identify unmatched from back-match
-back_unmatched <- post_census_matched %>%
-  filter(!three_key %in% back_matched$three_key)
-
-cat("❌ Records found in post_census_2022_building but NOT found in test2 using 3-key: ", nrow(back_unmatched), "\n")
-
-write_csv(back_unmatched, "back_unmatched_not_in_test2.csv")
-
-
-
-
-# ------------------------------
-# Final verification
-# ------------------------------
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#13 Final verification
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
 
 cat("✅ Verifying final sample count in test2...\n")
 final_check <- dbGetQuery(conn_surveys, glue("SELECT COUNT(*) FROM {db_table} WHERE sampled = '1';"))
 cat(glue("✅ Final count of sampled buildings in test2: {final_check[[1]]} (should be 2967)\n"))
 
 
-# ------------------------------
-# Clean up
-# ------------------------------
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
+#14 Clean up
+#░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░#
 
 dbDisconnect(conn_surveys)
 dbDisconnect(conn_census)
